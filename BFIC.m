8ea0dabcfc1c8201afbb2aab60a0f87156701db6
Elmin Sejdic <57618773+ElminS6@users.noreply.github.com>
2024-02-04 15:30:21 +0100

    Add files via upload

--- BFIC.m
+++ BFIC.m
@@ -1,19 +1,18 @@
 clc; clear all; close all;
 
-% UCITAVANJE SLIKE
-fname=uigetfile('*.gif');% otvara ui za odabranu sliku
-I=imread(fname); % ?ita odabranu sliku
-I=imresize(I,[256 256]);% zbog lakše komputacije, smanjuje sliku na 256*256
-figure,imshow(I);title('Originalna slika');drawnow; % prikaz slike
-vel=size(I); % veli?ina slike
-R=8; % blokovi kodomena (4x4, 8x8)
-D=2*R; % blokovi domena duplo veci
-step=8; % korak pri dijeljenju slike na blokove domena (4, 8)
-n=R*R; % broj piksela u jednom bloku slike
+% IMAGE LOADING
+fname=uigetfile('*.gif');% opens ui for the chosen image
+I=imread(fname); % % reads chosen image
+I=imresize(I,[256 256]);% resizing image to 256*256 for better computation
+figure,imshow(I);title('Originalna slika');drawnow; % image display
+R=16; % range blocks (4x4, 8x8)
+D=2*R; % domain blocks (2x range)
+step=8; % step when dividing the image into domain blocks (4, 8)
+n=R*R; %  number of pixels in one image block
 
-% DFINISANJE MATRICA I PRORACUN AFFINE TRANSFORMACIJA
-tx = 0;            % translacija po x osi
-ty = 0;            % translacija po y osi
+% DEFINITION OF MATRICES AND CALCULATION OF AFFINE TRANSFORMATIONS
+tx = 0;            % x-axis translation
+ty = 0;            % y-axis translation
 
 T{1} = [1  0  0; 0  1  0; tx  ty  1]; 
 T{2} = [-1  0  0; 0  1  0; tx  ty  1]; 
@@ -28,107 +27,107 @@ for i=1:length(T)
     aff{i}=affine2d(T{i});
 end  
 
-% PODJELA SLIKE NA BLOKOVE KODOMENE R
-
-br1=1;
+% DIVIDING IMAGE INTO RANGE BLOCKS R
+n1=1;
 for i=1:R:256
     for j=1:R:256
-        blok=I(i:i+R-1,j:j+R-1);
-        Range{br1}=blok;
-        br1=br1+1;
+        block=I(i:i+R-1,j:j+R-1);
+        Range{n1}=block;
+        n1=n1+1;
     end
 end
 
-% PODJELA SLIKE NA BLOKOVE DOMENE D
-
-br2=1;
-br3=1;
+% DIVIDING IMAGE INTO DOMAIN BLOCKS D
+n2=1;
+n3=1;
 for k=1:step:256-D+1
     for h=1:step:256-D+1
-            blok2=I(k:k+D-1,h:h+D-1);
-            %Domain{br2}=skaliranje(blok2, 2, 2); % skaliranje bloka D na velicinu R
-            Domain{br2}=imresize(blok2, [R R]);
-        % PRIMJENA AFFINE TRANSFORMACIJA NA BLOKOVE DOMENA D
-        % Na blokove domena nakon decimiranja izvršeno je 8 geometrijskih
-        % afinih transformacija, te je broj blokova domena 8*D_blokova.
+            block2=I(k:k+D-1,h:h+D-1);
+            Domain{n2}=imresize(block2, [R R]); % scaling of D blocks to size of R
+        % Applying AFFINE Transformations on domain blocks D
+        % 8 geometric affine transformations were performed on the domain blocks 
+        % after scaling, so the number of domain blocks is 8*D_blocks.
         for pp=1:length(T)
-            Newdomain{br3}=imwarp(Domain{br2},aff{pp});
-            af_odg{br3}=aff{pp}; %spremanje affinih transf.
-            br3=br3+1;
+            Newdomain{n3}=imwarp(Domain{n2},aff{pp});
+            af_corr{n3}=aff{pp}; % saving affine transf.
+            n3=n3+1;
         end
-            br2=br2+1;
+            n2=n2+1;
     end
 end
 
-% PRORACUN KOEFICIJENATA BRIGHTNESS I CONTRAST (si, oi)
-% Nakon primjene affinih transformacija izracunati su koeficijenti
-% kontrasta si i osvjetljenja oi. Dakle, za svaki blok R i skup blokova
-% domena nakon afinih transformacija, odredio se par koeficijenata.
+% CALCULATION OF BRIGHTNESS AND CONTRAST COEFFICIENTS (oi, si)
+% After applying affine transformations and dividing blocks into classes
+% the coefficients of contrast si and brightness oi were calculated. 
+% Thus, for each block R and the set of domain blocks after affine
+% transformations, a pair of coefficients was determined.
 
-% KODIRANJE
+% CODING
 tic;
-brmse=1;
-br1=0;
+nummse=1;
+n1=0;
 for a=1:length(Range)
     for b=1:length(Newdomain)
         Ra=double(Range{a});
         Db=double(Newdomain{b});
-        % sume u formulama za si i oi su izracunate pojedinacno radi
-        % jednostavnosti
+        % sums in the formulas for si & oi are calculated individually for simplicity
         sum1=sum(sum(Ra.*Db));
         sum2=sum(sum(Db))*sum(sum(Ra));
         sum3=sum(sum(Db.^2));
         sum4=(sum(sum(Db)))^2;
-        % formula za si:
+        % formula for si:
         s=((n)*(sum1)-sum2)/((n)*sum3-sum4);
-        % formula za oi:
+        % formula for oi:
         o=(sum(sum(Ra))-s*sum(sum(Db)))/((n));
         mat=ones(size(Ra)).*o;
         Dom{b}=uint8(Db.*s+mat);
-        mse=immse(Range{a},Dom{b}); % proracun mse blokova R i transformisanih
-        % blokova D, gdje su uracunati i parametri kontrasta i osvjetljenja
-        err(b)=mse; % spremanje MSE
-        koef{b}=[s o]; %spremanje para koeficijenata [si oi] u niz
-        brmse=brmse+1; % broj poredjenja blokova
+        % calculation of MSE for blocks R and transformed blocks D, where 
+        % the parameters of contrast and brightness are taken into account
+        mse=immse(Range{a},Dom{b}); 
+        err(b)=mse; % saving MSE
+        coeff{b}=[s o]; % saving coefficients [si oi] into an array
+        nummse=nummse+1; % number of block comparisons
     end
-    [M,In]=min(err); % pronalazenje minimalne MSE, te odgovarajuceg bloka D
-    % na osnovu indexa In pronasao se odgovarajuci transformisan blok D,
-    % koji je najslicniji bloku R.
-    aff_rekons{a}=af_odg{In}; % spremanje odgovarajuce affine transf.
-    koordinate(a)=ceil(In/8); % pozicija bloka D prije affinih transf.
-    s_o{a}=koef{In}; % spremanje optimalnih blokova si oi
+    % finding the minimum MSE, and the corresponding block D based on the
+    % index In, the corresponding transformed block D, which is the most 
+    % similar to the block R, was found.
+    [M,In]=min(err); 
+    aff_recons{a}=af_corr{In}; % saving corresponding affine transf.
+    coordinates(a)=ceil(In/8); % position of D block before affine transf.
+    s_o{a}=coeff{In}; % saving optimal blocks si & oi
 end
 % 
-Vrijeme_kodiranja=toc
-Broj_poredjenja=brmse-1
+Coding_time=toc
+Comparisons=nummse-1
 
 % DEKODIRANJE
-% Na osnovu transformisanih blokova D pronasle su se afine transformacije
-% i koeficijentima si i oi koji daju najmanju MSE za odg. blokove R i D.
-% Spremljeni optimalni koef. i afine transformacije primijenjeni su na 
-% inicijalnu sliku kako bi se formirala rekonstruisana slika.
+% Based on the transformed blocks D, affine transformations were found with
+% the coefficients si & oi, which give the smallest MSE for corresponding 
+% blocks R and D. Saved optimal coeff. and affine transformations were applied
+% to the initial image to form the reconstructed image.
 
-fname=uigetfile('*.gif');% otvara ui za inicijalnu sliku
-P=imread(fname); % cita odabranu sliku
-RekonsI=P; % inicijalna slika
+fname=uigetfile('*.gif');% opens ui for initial image
+P=imread(fname); % reads chosen image
+ReconsI=P; % initial image
 
 
 for it=1:15 
-    broj2=1;
+    num2=1;
     for kk=1:step:256-D+1
         for hh=1:step:256-D+1
-                blokk=RekonsI(kk:kk+D-1,hh:hh+D-1);
-                Domain_dekod{broj2}=imresize(blokk, [R R]);
-                broj2=broj2+1;
+                blockk=ReconsI(kk:kk+D-1,hh:hh+D-1);
+                Domain_dekod{num2}=imresize(blockk, [R R]);
+                num2=num2+1;
         end
     end
     for f=1:length(Range)
-    RekonsIm=imwarp(Domain_dekod{koordinate(f)},aff_rekons{f}); % primjena affine transformacije
-    mat1=ones(size(RekonsIm)).*s_o{f}(2); 
-    % optimizacija kontrasta i osvjetljenja na osnovu si i oi
-    ReI{f}=imresize(uint8(double(RekonsIm).*s_o{f}(1)+mat1), [R R]);
+    % applying affine transformation
+    ReconsIm=imwarp(Domain_dekod{coordinates(f)},aff_recons{f});
+    mat1=ones(size(ReconsIm)).*s_o{f}(2); 
+    % optimization of contrast and brightness based on si and oi
+    ReI{f}=imresize(uint8(double(ReconsIm).*s_o{f}(1)+mat1), [R R]);
     end
-    % Rekonstrukcija slike
+    % Image reconstruction
     p=1;
     for q=1:length(I)/R
      for v=1:length(I)/R
@@ -137,15 +136,15 @@ for it=1:15
      end
     end
     it
-    RekonsI=cell2mat(oldimg); % slika u sljedecoj iteraciji
+    ReconsI=cell2mat(oldimg); % image in the next iteration
     if (it==1 || it==2 || it==3 || it==5)
-        figure, imshow(RekonsI);
+        figure, imshow(ReconsI);
     end
 end
 
 
 
-% PRIKAZ REKONSTRUISANE SLIKE
-figure, imshow(RekonsI); title('Rekonstruisana slika');drawnow;
-[peaksnr,~] = psnr(RekonsI,I);
+% DISPLAY OF RECONSTRUCTED IMAGE
+figure, imshow(ReconsI); title('Rekonstruisana slika');drawnow;
+[peaksnr,~] = psnr(ReconsI,I);
 PSNR=peaksnr
--- FICdct.m
+++ FICdct.m
@@ -1,19 +1,18 @@
 clc; clear all; close all;
 
-% UCITAVANJE SLIKE
-fname=uigetfile('*.gif');% otvara ui za odabranu sliku
-I=imread(fname); % cita odabranu sliku
-I=imresize(I,[256 256]);% zbog lakše komputacije, smanjuje sliku na 256*256
-figure,imshow(I);title('Originalna slika');drawnow; % prikaz slike
-R=8; % blokovi kodomena (4x4, 8x8)
-D=2*R; % blokovi domena duplo veci
-step=8; % korak pri dijeljenju slike na blokove domena (4, 8)
-n=R*R; % broj piksela u jednom bloku slike
+% IMAGE LOADING
+fname=uigetfile('*.gif');% opens ui for the chosen image
+I=imread(fname); % reads chosen image
+I=imresize(I,[256 256]);% resizing image to 256*256 for better computation
+figure,imshow(I);title('Originalna slika');drawnow; % image display
+R=8; % range blocks (4x4, 8x8)
+D=2*R; % domain blocks (2x range)
+step=8; % step when dividing the image into domain blocks (4, 8)
+n=R*R; % number of pixels in one image block
 
-% DFINISANJE MATRICA I PRORAcUN AFFINE TRANSFORMACIJA
-tx = 0;            % translacija po x osi
-ty = 0;            % translacija po y osi
-
+% DEFINITION OF MATRICES AND CALCULATION OF AFFINE TRANSFORMATIONS
+tx = 0;            % x-axis translation
+ty = 0;            % y-axis translation
 T{1} = [1  0  0; 0  1  0; tx  ty  1]; 
 T{2} = [-1  0  0; 0  1  0; tx  ty  1]; 
 T{3} = [1  0  0; 0  -1  0; tx  ty  1]; 
@@ -27,68 +26,70 @@ for i=1:length(T)
     aff{i}=affine2d(T{i});
 end
 
-% PODJELA SLIKE NA BLOKOVE KODOMENE R
-
-br1=1;
+% DIVIDING IMAGE INTO RANGE BLOCKS R
+num1=1;
 for i=1:R:256
     for j=1:R:256
-    blok=I(i:i+R-1,j:j+R-1);
-    Range{br1}=blok;
-    br1=br1+1;
+    block=I(i:i+R-1,j:j+R-1);
+    Range{num1}=block;
+    num1=num1+1;
     end
 end
 
 
-% ODREDjIVANJE PRAGOVA Ts i Td
+% THRESHOLDS DETERMINATION Ts & Td
 l=1;
 for j=1:length(Range)  
-    dctkoef=dct2(Range{j}); % proracun dct koeficijenata
-    f10=abs(dctkoef(2,1));
-    f01=abs(dctkoef(1,2));
-    fovi{j}=[f10 f01]; %spremanje vrijednosti dct koef u niz
-    fz=max(f10,f01); % odredjivanje vrijednosti Fmax za prvi prag
+    dctcoeff=dct2(Range{j}); % DCT coefficients calculation
+    f10=abs(dctcoeff(2,1));
+    f01=abs(dctcoeff(1,2));
+    fovi{j}=[f10 f01]; % saving DCT values to an array 
+    fz=max(f10,f01); % value Fmax for the first threshold
     fzzz(j)=fz;
 end
 [fzzz,r]=sort(fzzz);
-c=round(length(Range)/3); % vrijednost na prvoj trecini odgovara pragu TS
+% the value in the first third corresponds to the TS threshold
+c=round(length(Range)/3); 
 Ts=fzzz(c);
 l=1;
 
 for b=c+1:length(Range)
-    fk=abs(fovi{r(b)}(1)-fovi{r(b)}(2)); %proracun apsolutne vrijednosti za odredjivanje drugog praga
+    % calculation of the absolute value for determining the second threshold
+    fk=abs(fovi{r(b)}(1)-fovi{r(b)}(2));
     fkkk(l)=fk; 
     l=l+1;
 end    
 fkkk=sort(fkkk);
-Td=fkkk(round(length(fkkk)/2)); %vrijednost na polovini preostalih blokova odgovara pragu TD
+% the value on half of the remaining blocks corresponds to the TD threshold
+Td=fkkk(round(length(fkkk)/2));
 
 sr=1;
 dr=1;
 hr=1;
 
-% klasifikacija blokova R na klase S,D,H
+% Range blocks classification into classes S,D,H
 Newrange=Range;
 for qq=1:length(Range)  
-    dctkoef=dct2(Range{qq});
-    f10=abs(dctkoef(2,1));
-    f01=abs(dctkoef(1,2));
+    dctcoeff=dct2(Range{qq});
+    f10=abs(dctcoeff(2,1));
+    f01=abs(dctcoeff(1,2));
     if f10<Ts && f01<Ts
         typeSr{sr}=Range{qq};
         sr=sr+1;
-        Newrange{qq}(1:R,1:R)=0; % blokovi S obojeni u crno
+        Newrange{qq}(1:R,1:R)=0; % S blocks - black
     else if abs(f10-f01)<Td
             typeDr{dr}=Range{qq};
             dr=dr+1;
-            Newrange{qq}(1:R,1:R)=500; % blokovi D obojeni u bijelo
+            Newrange{qq}(1:R,1:R)=500; % D blocks - white
         else
             typeHr{hr}=Range{qq};
             hr=hr+1;
-            Newrange{qq}(1:R,1:R)=100; % blokovi H obojeni u sivo
+            Newrange{qq}(1:R,1:R)=100; % H blocks - gray
         end
     end
 end
 
-% prikaz slike pomocu klasificiranih blokova
+% image display using classified blocks
 pr=1;
 for qr=1:256/R
   for vr=1:256/R
@@ -97,180 +98,177 @@ for qr=1:256/R
      end
 end
 Pr=cell2mat(oldimgr);
-figure; imshow(Pr); title('Slika blokova S, D i H');drawnow;
+figure; imshow(Pr); title('Slika blockova S, D i H');drawnow;
 
 
-% PODJELA SLIKE NA BLOKOVE DOMENE D
-
-br2=1;
-br3=1;
+% DIVIDING IMAGE INTO DOMAIN BLOCKS D
+num2=1;
+num3=1;
 for k=1:step:256-D+1
     for h=1:step:256-D+1
-            blok2=I(k:k+D-1,h:h+D-1);
-            Domain{br2}=imresize(blok2,[R R]); % smanjenje velicine bloka D na velicinu R (poduzorkovanje)
-        
-        % PRIMJENA AFFINE TRANSFORMACIJA NA BLOKOVE DOMENA D
+            block2=I(k:k+D-1,h:h+D-1);
+        % reducing the block size D to the size R (subsampling)
+            Domain{num2}=imresize(block2,[R R]);
+        % Applying AFFINE Transformations on domain blocks D
         for pp=1:length(T)
-            Newdomain{br3}=imwarp(Domain{br2},aff{pp});
-            aff_odg{br3}=aff{pp}; %spremanje affinih transf.
-            br3=br3+1;
+            Newdomain{num3}=imwarp(Domain{num2},aff{pp});
+            aff_corr{num3}=aff{pp}; % Saving Affine Transformations
+            num3=num3+1;
         end
-        br2=br2+1;
+        num2=num2+1;
     end
 end
 
-% RASPODJELA BLOKOVA DOMENE U 3 KLASE: S, D I H
+% DISTRIBUTION OF DOMAIN BLOCKS INTO 3 CLASSES: S, D & H
 s=1;
 h=1;
 d=1;
 for q=1:length(Newdomain)  
-    dctkoef=dct2(Newdomain{q});
-    f10=abs(dctkoef(2,1));
-    f01=abs(dctkoef(1,2));
+    dctcoeff=dct2(Newdomain{q});
+    f10=abs(dctcoeff(2,1));
+    f01=abs(dctcoeff(1,2));
     f{q}=[f10 f01];
-    % ispitivanje uslova 
+    % testing of conditions
     if f10<Ts && f01<Ts
         typeS{s}=Newdomain{q};
-        affS{s}=aff_odg{q};
-        koorS(s)=q;
+        affS{s}=aff_corr{q};
+        coorS(s)=q;
         s=s+1;
     else if abs(f10-f01)<Td
             typeD{d}=Newdomain{q};
-            affD{d}=aff_odg{q};
-            koorD(d)=q;
+            affD{d}=aff_corr{q};
+            coorD(d)=q;
             d=d+1;
         else
             typeH{h}=Newdomain{q};
-            affH{h}=aff_odg{q};
-            koorH(h)=q;
+            affH{h}=aff_corr{q};
+            coorH(h)=q;
             h=h+1;
         end
     end
 end
 
-% PRORACUN KOEFICIJENATA BRIGHTNESS I CONTRAST (si, oi)
-% Nakon primjene affinih transformacija i podjele blokova u klase
-% izracunati su koeficijenti kontrasta si i osvjetljenja oi. 
-% Dakle, u ovom slucaju koeficijenti su izracunati za one blokove R i
-% transformisane blokove D koji pripadaju istoj klasi (S, D ili H).
-% Na osnovu toga odredjeni su si i oi koef. za blokove klasa S, D i H.
+% CALCULATION OF BRIGHTNESS AND CONTRAST COEFFICIENTS (oi, si)
+% After applying affine transformations and dividing blocks into classes
+% the coefficients of contrast si and brightness oi were calculated.
+% So, in this case, the coefficients were calculated for those blocks R and
+% transformed blocks D belonging to the same class (S, D or H).
+% Based on this, si and oi coefficients were determined for blocks of classes S, D and H.
 
-% KODIRANJE FIC-DCT: POREDjENJE BLOKOVA IZ ODGOVARAJUCIH KLASA, TE
-% PRONALAZAK NAJSLICNIJEG DOMENSKOG BLOKA SA OPTIMALNIM KOEF. si i oi i
-% afinom transform., tako da zadovoljava min vrijednost MSE.
+% FIC-DCT CODING: COMPARISON OF BLOCKS FROM CORRESPONDING CLASSES, AND
+% FINDING THE MOST SIMILAR DOMAIN BLOCK WITH THE OPTIMUM COEF. si & oi &
+% affine transform., so that it meets the min value of MSE.
 
-brmse=1;
-brmse1=0;
-brmse2=0;
-brmse3=0;
+nummse=1;
+nummse1=0;
+nummse2=0;
+nummse3=0;
 tic;
 for a=1:length(Range)
-    % Poredjenje blokova R sa blokovima D iz odgovarajuce klase
-    % te traženje minimalne MSE na osnovu primjenjenih tranformacija i si
-    % oi koef.
+     % Comparison of blocks R with blocks D from the corresponding class
+     % and searching for the minimum MSE based on the applied transformations and si
+     % & oi coef.
     if fovi{a}(1)<Ts && fovi{a}(2)<Ts
         for sp=1:length(typeS)
             Ra=double(Range{a});
             Db=double(typeS{sp});
-            % sume u formulama za si i oi su izracunate pojedinacno radi
-            % jednostavnosti
+            % sums in the formulas for si & oi are calculated individually for simplicity
             sum1=sum(sum(Ra.*Db));
             sum2=sum(sum(Db))*sum(sum(Ra));
             sum3=sum(sum(Db.^2));
             sum4=(sum(sum(Db)))^2;
-            % formula za si:
+            % formula for si:
             s=((n)*(sum1)-sum2)/((n)*sum3-sum4);
-            % formula za oi:
+            % formula for oi:
             o=(sum(sum(Ra))-s*sum(sum(Db)))/((n));
             mat=ones(size(Ra)).*o;    
             mse=immse(Range{a},uint8(Db.*s+mat));
             err1(sp)=mse;
-            koefS{sp}=[s o];
-            brmse1=brmse1+1;
+            coeffS{sp}=[s o];
+            nummse1=nummse1+1;
         end
         [M,In]=min(err1);
-        aff_rekons{a}=affS{In}; % spremanje odgovarajuce affine transf.
-        koordinate(a)=ceil(koorS(In)/8); % pozicija bloka D prije affinih transf.
-        s_o{a}=koefS{In}; % spremanje optimalnih blokova si oi
+        aff_recons{a}=affS{In}; % saving corresponding affine transf.
+        coordinates(a)=ceil(coorS(In)/8); % D block position before affine transf.
+        s_o{a}=coeffS{In}; % saving optimal blocks si & oi
     else if abs(fovi{a}(1)-fovi{a}(2))<Td
         for dp=1:length(typeD)
             Ra=double(Range{a});
             Db=double(typeD{dp});
-            % sume u formulama za si i oi su izracunate pojedinacno radi
-            % jednostavnosti
+            % sums in the formulas for si & oi are calculated individually for simplicity
             sum1=sum(sum(Ra.*Db));
             sum2=sum(sum(Db))*sum(sum(Ra));
             sum3=sum(sum(Db.^2));
             sum4=(sum(sum(Db)))^2;
-            % formula za si:
+            % formula for si:
             s=((n)*(sum1)-sum2)/((n)*sum3-sum4);
-            % formula za oi:
+            % formula for oi:
             o=(sum(sum(Ra))-s*sum(sum(Db)))/((n));
             mat=ones(size(Ra)).*o;    
             mse=immse(Range{a},uint8(Db.*s+mat));
             err2(dp)=mse;
-            koefD{dp}=[s o];
-            brmse2=brmse2+1;
+            coeffD{dp}=[s o];
+            nummse2=nummse2+1;
         end
         [M,In]=min(err2);
-        aff_rekons{a}=affD{In}; % spremanje odgovarajuce affine transf.
-        koordinate(a)=ceil(koorD(In)/8); % pozicija bloka D prije affinih transf.
-        s_o{a}=koefD{In}; % spremanje optimalnih blokova si oi
+        aff_recons{a}=affD{In}; % saving corresponding affine transf.
+        coordinates(a)=ceil(coorD(In)/8); % D block position before affine transf.
+        s_o{a}=coeffD{In}; % saving optimal blocks si & oi
         else
         for hp=1:length(typeH)
             Ra=double(Range{a});
             Db=double(typeH{hp});
-            % sume u formulama za si i oi su izracunate pojedinacno radi
-            % jednostavnosti
+            % sums in the formulas for si & oi are calculated individually for simplicity
             sum1=sum(sum(Ra.*Db));
             sum2=sum(sum(Db))*sum(sum(Ra));
             sum3=sum(sum(Db.^2));
             sum4=(sum(sum(Db)))^2;
-            % formula za si:
+            % formula for si:
             s=((n)*(sum1)-sum2)/((n)*sum3-sum4);
-            % formula za oi:
+            % formula for oi:
             o=(sum(sum(Ra))-s*sum(sum(Db)))/((n));
             mat=ones(size(Ra)).*o;    
             mse=immse(Range{a},uint8(Db.*s+mat));
             err3(hp)=mse;
-            koefH{hp}=[s o];
-            brmse3=brmse3+1;
+            coeffH{hp}=[s o];
+            nummse3=nummse3+1;
         end
         [M,In]=min(err3);
-        aff_rekons{a}=affH{In}; % spremanje odgovarajuce affine transf.
-        koordinate(a)=ceil(koorH(In)/8); % pozicija bloka D prije affinih transf.
-        s_o{a}=koefH{In}; % spremanje optimalnih blokova si oi
+        aff_recons{a}=affH{In}; % saving corresponding affine transf.
+        coordinates(a)=ceil(coorH(In)/8); % D block position before affine transf.
+        s_o{a}=coeffH{In}; % saving optimal blocks si & oi
         end
     end
             
 end
-brmse=brmse1+brmse2+brmse3;
-Vrijeme_kodiranja=toc
-Broj_poredjenja=brmse
+nummse=nummse1+nummse2+nummse3;
+Coding_time=toc
+Comparisons=nummse
 
-% % DEKODIRANJE
+% DECODING
 
-fname=uigetfile('*.gif');% otvara ui za inicijalnu sliku
-P=imread(fname); % cita odabranu sliku
-RekonsI=P; % inicijalna slika
+fname=uigetfile('*.gif');% opens ui for initial image
+P=imread(fname); % reads chosen image
+reconsI=P; % initial image
 
 
 for it=1:15
-    broj2=1;
+    numb2=1;
     for kk=1:step:256-D+1
         for hh=1:step:256-D+1
-                blokk=RekonsI(kk:kk+D-1,hh:hh+D-1);
-                Domain_dekod{broj2}=imresize(blokk, [R R]);
-                broj2=broj2+1;
+                blockk=reconsI(kk:kk+D-1,hh:hh+D-1);
+                Domain_decod{numb2}=imresize(blockk, [R R]);
+                numb2=numb2+1;
         end
     end
     for f=1:length(Range)
-    RekonsIm=imwarp(Domain_dekod{koordinate(f)},aff_rekons{f}); % primjena affine transformacije
-    mat1=ones(size(RekonsIm)).*s_o{f}(2); 
-    % optimizacija kontrasta i osvjetljenja na osnovu si i oi
-    ReI{f}=imresize(uint8(double(RekonsIm).*s_o{f}(1)+mat1), [R R]);
+    % applying affine transformation
+    reconsIm=imwarp(Domain_decod{coordinates(f)},aff_recons{f});
+    mat1=ones(size(reconsIm)).*s_o{f}(2); 
+    % optimization of contrast and brightness based on si and oi
+    ReI{f}=imresize(uint8(double(reconsIm).*s_o{f}(1)+mat1), [R R]);
     end
-    % Rekonstrukcija slike
+    % Image reconstruction
     p=1;
     for q=1:length(I)/R
      for v=1:length(I)/R
@@ -279,13 +277,12 @@ for it=1:15
      end
     end
     it
-    RekonsI=cell2mat(oldimg); % slika u sljedecoj iteraciji
+    reconsI=cell2mat(oldimg); % image in the next iteration
 end
 
 
 
-% PRIKAZ REKONSTRUISANE SLIKE
-
-figure, imshow(RekonsI); title('Rekonstruisana slika');drawnow;
-[peaksnr,~] = psnr(RekonsI,I);
+% DISPLAY OF RECONSTRUCTED IMAGE
+figure, imshow(reconsI); title('reconstruisana slika');drawnow;
+[peaksnr,~] = psnr(reconsI,I);
 PSNR=peaksnr
